18fadd1a58209b42624fe54345bc52adbb26ffdf
AbdelrahmanShaheen <shaheenabdelrahman28@gmail.com>
2024-02-23 01:02:52 +0200

    testing setup - using testcontainers for testing

---
+++ backend/src/test/java/com/shaheen/AbstractTestContainers.java
@@ -0,0 +1,42 @@
+package com.shaheen;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+public abstract class AbstractTestContainers {
+    @Container
+    protected static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("shaheen-dao-unit-test")
+                    .withUsername("shaheen")
+                    .withPassword("shaheen");
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry){
+        registry.add(
+                "spring.datasource.url",
+                postgreSQLContainer::getJdbcUrl
+        );
+        registry.add(
+                "spring.datasource.username",
+                postgreSQLContainer::getUsername
+        );
+        registry.add(
+                "spring.datasource.password",
+                postgreSQLContainer::getPassword
+        );
+    }
+    @BeforeAll
+    static void migrationShouldBeApplied(){
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
+        System.out.println();
+    }
+}
